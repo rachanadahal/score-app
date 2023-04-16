8fb2e21759273f13c9c9d670b10d48c38e82241c
Rachana Dahal <rachanadahal12@gmail.com>
2023-04-16 22:24:02 +0545

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,26 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp());
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+  ));
+}
+
+class ScoreApp extends StatelessWidget {
+  // const ScoreApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Score App",
+      home: ScoreHome(),
+    );
+  }
 }
 
 class ScoreHome extends StatefulWidget {
+  // const ScoreHome({super.key});
+
   @override
   State<ScoreHome> createState() => _ScoreHomeState();
 }
@@ -14,7 +30,7 @@ class _ScoreHomeState extends State<ScoreHome> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Score"),
+        title: const Text("Score changer"),
         centerTitle: true,
       ),
     );
